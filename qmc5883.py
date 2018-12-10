bf0cff433333cbc1350d733a087e1383bf58c6d6
hh <robert@hammelrath.com>
2018-12-10 17:58:32 +0100

    qmc5883.py: define constants with const()
    That saves code space & execution time. The symbol access is replaced by direct
    assignment. For Python variants not supporting const a lambda function is added.

--- qmc5883.py
+++ qmc5883.py
@@ -17,58 +17,62 @@
 
 import time
 import struct
-from machine import idle
-
 
 class QMC5883:
+    # probe the existence of const()
+    try:
+        _canary = const(0)
+    except:
+        const = lambda x:x
+
     # Default I2C address
-    ADDR = 0x0D
+    ADDR = const(0x0D)
 
     # QMC5883 Register numbers
-    X_LSB = 0
-    X_MSB = 1
-    Y_LSB = 2
-    Y_MSB = 3
-    Z_LSB = 4
-    Z_MSB = 5
-    STATUS = 6
-    T_LSB = 7
-    T_MSB = 8
-    CONFIG = 9
-    CONFIG2 = 10
-    RESET = 11
-    STATUS2 = 12
-    CHIP_ID = 13
+    X_LSB = const(0)
+    X_MSB = const(1)
+    Y_LSB = const(2)
+    Y_MSB = const(3)
+    Z_LSB = const(4)
+    Z_MSB = const(5)
+    STATUS = const(6)
+    T_LSB = const(7)
+    T_MSB = const(8)
+    CONFIG = const(9)
+    CONFIG2 = const(10)
+    RESET = const(11)
+    STATUS2 = const(12)
+    CHIP_ID = const(13)
 
     # Bit values for the STATUS register
-    STATUS_DRDY = 1
-    STATUS_OVL = 2
-    STATUS_DOR = 4
+    STATUS_DRDY = const(1)
+    STATUS_OVL = const(2)
+    STATUS_DOR = const(4)
 
     # Oversampling values for the CONFIG register
-    CONFIG_OS512 = 0b00000000
-    CONFIG_OS256 = 0b01000000
-    CONFIG_OS128 = 0b10000000
-    CONFIG_OS64 = 0b11000000
+    CONFIG_OS512 = const(0b00000000)
+    CONFIG_OS256 = const(0b01000000)
+    CONFIG_OS128 = const(0b10000000)
+    CONFIG_OS64 = const(0b11000000)
 
     # Range values for the CONFIG register
-    CONFIG_2GAUSS = 0b00000000
-    CONFIG_8GAUSS = 0b00010000
+    CONFIG_2GAUSS = const(0b00000000)
+    CONFIG_8GAUSS = const(0b00010000)
 
     # Rate values for the CONFIG register
-    CONFIG_10HZ = 0b00000000
-    CONFIG_50HZ = 0b00000100
-    CONFIG_100HZ = 0b00001000
-    CONFIG_200HZ = 0b00001100
+    CONFIG_10HZ = const(0b00000000)
+    CONFIG_50HZ = const(0b00000100)
+    CONFIG_100HZ = const(0b00001000)
+    CONFIG_200HZ = const(0b00001100)
 
     # Mode values for the CONFIG register
-    CONFIG_STANDBY = 0b00000000
-    CONFIG_CONT = 0b00000001
+    CONFIG_STANDBY = const(0b00000000)
+    CONFIG_CONT = const(0b00000001)
 
     # Mode values for the CONFIG2 register
-    CONFIG2_INT_DISABLE = 0b00000001
-    CONFIG2_ROL_PTR = 0b01000000
-    CONFIG2_SOFT_RST = 0b10000000
+    CONFIG2_INT_DISABLE = const(0b00000001)
+    CONFIG2_ROL_PTR = const(0b01000000)
+    CONFIG2_SOFT_RST = const(0b10000000)
 
     def __init__(self, i2c, offset=50.0):
         self.i2c = i2c
@@ -129,7 +133,7 @@ class QMC5883:
     def read_raw(self):
         try:
             while not self.ready():
-                idle()
+                time.sleep(0.005)
             self.i2c.readfrom_mem_into(QMC5883.ADDR, QMC5883.X_LSB, self.register)
         except OSError as error:
             print("OSError", error)
